bb9132e8a82800f226e74d7dbb14d65445f502f8
Rushi Sharma <rushisharma64@gmail.com>
2024-06-09 19:20:31 -0700

    Spring Boot Practice

---
+++ src/main/java/com/rushi/MyWebApp/Alien.java
@@ -0,0 +1,31 @@
+package com.rushi.MyWebApp;
+
+public class Alien {
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    private int aid;
+    private String aname;
+    private String lang;
+}
